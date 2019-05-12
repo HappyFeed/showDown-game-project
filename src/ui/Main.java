702919636669112544e25cba50c23a96dec8adb0
ProtSenpai <jdjimenez04@hotmail.com>
2019-05-12 12:46:54 -0500

    fxml of menu and controller, and updated interfaces

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -16,9 +16,9 @@ public class Main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception 
 	{
-        Parent root=FXMLLoader.load(getClass().getResource("Match.fxml"));
+        Parent root=FXMLLoader.load(getClass().getResource("Menu.fxml"));
 		Scene scene= new Scene(root);
-		stage.setTitle("jisus");
+		stage.setTitle("Pokemon Showdown v2");
 		stage.setScene(scene);
 		stage.show();
 	}
